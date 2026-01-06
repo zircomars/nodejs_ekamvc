453676fef77cd4fb55eb3c724051127ff66efa97
Zhao <61022230+zircomars@users.noreply.github.com>
2026-01-06 17:28:04 +0200

    Add files via upload

---
+++ NodejsekaMVC/index.js
@@ -0,0 +1,21 @@
+/* ohjeet ja tutoriaalit mitä voi seurata step by step::
+
+https://www.youtube.com/watch?v=Cgvopu9zg8Y
+
+*/
+//Tämä on pääsovellus
+const express = require("express");
+const app = express();
+const routes = require("./routes/routes");
+const PORT = 8080;
+
+//lukaisee polku & käsittelee tiedosto kansion -> routes/routes.js
+app.use("/api", routes);
+
+//default home page
+app.use("/", (req, res) => {
+  res.send("Default page");
+});
+
+
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
---
+++ NodejsekaMVC/routes/routes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+const controller = require("../controllers/UserController");
+
+/*
+router
+.route("/")
+.get(function(req, res){
+  res
+.status(200)
+.send("This is routes/routes path ");
+console.log("Routes path");
+});
+*/
+
+
+router.get("/", controller.get);
+
+  //res.send("This is router page");
+
+
+
+module.exports = router;
